589379f424a7b29775f4c596762230792bf9bfc2
sceller <victorlopezse@gmail.com>
2018-08-03 23:08:16 -0400

    first commit

---
+++ MLModelManager.py
@@ -0,0 +1,23 @@
+# MLModelManager class definition
+class MLModelManager(object):
+
+    def __init__(self):
+        self.setups = []
+
+    # a setup must be a dictionary with at least 2 nodes: details, eval_metrics
+    def save_setup(self, setup):       
+            self.setups.append(setup)
+
+    # given a KPI (e.g. F1), get_best_model will iterate over all setups saved previously and return the highest one
+    def get_best_model(self,kpi,metrics_node):
+        max_kpi_value = 0.0
+
+        for setup in self.setups:
+            if kpi in setup[metrics_node]:
+                kpi_value = setup[metrics_node][kpi]
+
+                if kpi_value > max_kpi_value:
+                    max_kpi_value = kpi_value
+                    setup_to_return = setup
+
+            return setup_to_return
